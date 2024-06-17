ae14dedad8c39a9e43dfc2b83b181777632ecf90
Samugcor <samueligcorp@gmail.com>
2024-06-17 14:27:42 +0200

    pruebas

--- script.js
+++ script.js
@@ -24,17 +24,31 @@ $(function () {
     });
 
     $("#searchbar").keyup(function() {
-        
+        emptycards()
         let input = $(this).val().toLowerCase();
 
-        $(".cardcontainer .card").each(function() {
-            let nombre = $(this).find(".nombre").text().toLowerCase();
+        $.getJSON(`https://zelda.fanapis.com/api/games?limit=100`, function(response) {
+            listaJuegos = response.data;
+            let listaJuegosFiltro=[];
+            listaJuegos.forEach(juego => {
+                console.log(juego.name.includes(input))
+                if (juego.name.includes(input)==true) {
+                    listaJuegosFiltro.push(juego)
+                }
+            });
 
-            if (nombre.includes(input)) {
-                $(this).show();
-            } else {
-                $(this).hide();
-            }
+            console.log(listaJuegosFiltro);
+
+            let template = $("#cardTemplate").prop('content');
+    
+            listaJuegosFiltro.forEach((juego,index )=> {
+                let clone = $(template).clone(true);
+                $(clone).find(".card").data("indice", index);
+                $(clone).find(".nombre").text(juego.name);
+                $(clone).find(".fecha").text(juego.released_date);
+                $(clone).find(".descripcion").text(juego.description);
+                $(".cardcontainer").append(clone);
+            });
         });
     });
 })
